e89be8126ffc63d410a4b8ffc6c0ed2d19672390
HanawuZ <b6302245@gmail.com>
2023-06-09 23:40:24 +0700

    :sparkles: Add get restaurant by name function
    .

--- README.md
+++ README.md
@@ -16,3 +16,17 @@ NLP Project Line Chatbot with dialogflow webhook backend
 5. Copy this url and paste to Line bot Webhook URL
 
 6. Verify a Webhook URL
+
+### Setup backend
+1. Change directory to backend
+```cd backend```
+2. Run app.js using nodemon
+```nodemon app.js```
+3. Open server using Ngrok by opening HTTP port 3000
+```ngrok http 3000```
+4. Copy ngrok https url and concat "test" to url
+```https://<some_number>.ap.ngrok.io/test```
+
+5. Copy this URL and paste in Dialogflow fulfillment
+
+6. Save fulfillment configuration
--- backend/function/storesController.js
+++ backend/function/storesController.js
@@ -1,48 +1,69 @@
 const { Payload } = require("dialogflow-fulfillment");
 const { Op } = require("sequelize");
-const Store = require("../db/models/store.model");
-const { templates } = require("./templates/store.template");
+const Store = require('../db/models/store.model');
+const { templates } = require("../templates/store.template")
 
 // To find a resturant at nearby locations
 const ListNearStores = async (agent, req) => {
-  const coord = req.body.queryResult.parameters.number;
-  const latitude = coord[0];
-  const longtitude = coord[1];
+   const coord = req.body.queryResult.parameters.number;
+   const latitude = coord[0];
+   const longtitude = coord[1];
 
-  // Query the database for stores within a certain range of coordinates
-  const res = await Store.findAll({
-    where: {
-      lat: {
-        [Op.between]: [latitude - 0.2, latitude + 0.2],
+   // Query the database for stores within a certain range of coordinates
+   const res = await Store.findAll({
+      where: {
+         lat: {
+            [Op.between]: [latitude - 0.2, latitude + 0.2],
+         },
+         long: {
+            [Op.between]: [longtitude - 0.2, longtitude + 0.2],
+         },
       },
-      long: {
-        [Op.between]: [longtitude - 0.2, longtitude + 0.2],
-      },
-    },
-  });
+   });
 
-  // Extract the data values from the query results
-  const storeResults = [res[0].dataValues, res[1].dataValues];
+   // Extract the data values from the query results
+   const storeResults = [res[0].dataValues, res[1].dataValues];
 
-  // Create a payload with the store results
-  const payload = {
-    line: templates(storeResults),
-  };
+   // Create a payload with the store results
+   const payload = {
+      line: templates(storeResults),
+   };
 
-  // Add the payload to the agent's response
-  agent.add(
-    new Payload(agent.UNSPECIFIED, payload, {
-      rawPayload: true,
-      sendAsMessage: true,
-    })
-  );
+   // Add the payload to the agent's response
+   agent.add(
+      new Payload(agent.UNSPECIFIED, payload, {
+         rawPayload: true,
+         sendAsMessage: true,
+      })
+   );
 };
 
 
 // To show a restuarant of you
 const ShowRestuarant = async (agent, req) => {
+   const store_name = req.body.queryResult.parameters.store_name
 
-   agent.add("UUEIEJFIIOJEFOI")
+   const result = await Store.findOne({
+      where: {
+         store_name: store_name
+      }
+   })
+
+   const payload = {
+      "line": {
+         "type": "location",
+         "title": `ตำแหน่งร้าน ${result.store_name}`,
+         "address": "ได้ตำแหน่งร้านอาหารแล้ว",
+         "latitude": result.lat,
+         "longitude": result.long
+      }
+   }
+   agent.add(
+      new Payload(agent.UNSPECIFIED, payload, {
+         rawPayload: true,
+         sendAsMessage: true,
+      })
+   );
 }
 
 module.exports = { ListNearStores, ShowRestuarant };

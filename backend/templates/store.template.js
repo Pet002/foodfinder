f36817154f78a42725d38fec434479a494fbfb7c
Pet002 <pet.14336@gmail.com>
2023-06-09 21:35:21 +0700

    :fire: Firer code

--- backend/templates/store.template.js
+++ backend/templates/store.template.js
@@ -1,35 +1,33 @@
-const templates = {
+const templates = (datas) => ({
   type: "template",
   altText: "this is a carousel template",
   template: {
     type: "carousel",
-    columns: [array, array],
+    columns: carousel(datas),
   },
-};
-
-const array = {
-  title: "Title",
-  text: "Text",
-  actions: [
-    {
-      type: "message",
-      label: "Action 1",
-      text: "Action 1",
-    },
-    {
-      type: "message",
-      label: "Action 2",
-      text: "Action 2",
-    },
-  ],
-};
+})
 
 
-const carousel = (data) => {
-    data.map(() => {
-        
+const carousel = (datas) => {
+    return datas.map((data) => {
+        return {
+            title: data.store_name,
+            text: "Text",
+            actions: [
+              {
+                type: "message",
+                label: "Action 1",
+                text: "Action 1",
+              },
+              {
+                type: "message",
+                label: "Action 2",
+                text: "Action 2",
+              },
+            ],
+          }
     })
 }
 
 
-module.exports
+module.exports = {templates}
